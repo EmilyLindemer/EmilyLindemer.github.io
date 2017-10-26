6f31bf3aa0b6ebd2aaf7eca2b3672d566e565617
Emily Lindemer <emily@corticometrics.com>
2017-10-25 22:36:05 -0400

    test

---
+++ nationalparks/parks-info.js
@@ -0,0 +1,13 @@
+var parksinfo = "
+  {
+      "park":"Acadia",
+      "established":"1916",
+      "visited":"2015,2016,2017",
+      "lodging":"Blackwoods Campground",
+      "image":"acadia.png",
+      "hiking":"The Precipice Trail",
+      "impressions":"super cool",
+      "gettingthere":"drive a lot",
+      "ontheway":"so much cool stuff in bar harbor",
+      "secrets":"cottage st pub"
+  } "
